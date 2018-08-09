b458f018c43c83cb2130b329698dee5296e45fd5
erin-hughes <35696678+erin-hughes@users.noreply.github.com>
2018-08-09 15:35:33 +0100

    adding deploy_config and ping functionality (#3)
    Signed-off-by: Erin Hughes <Erin.Hughes@ibm.com>

--- test/chaincode.js
+++ test/chaincode.js
@@ -21,14 +21,37 @@ describe('Chaincode', () => {
     });
 
     describe('#Invoke', async () => {
+        let cc;
+        let stub;
+        let res;
 
-        it('should work', async () => {
-            const cc = new Chaincode();
-            const stub = sinon.createStubInstance(Stub);
+        beforeEach(async () => {
+            cc = new Chaincode();
+            stub = sinon.createStubInstance(Stub);
             stub.getFunctionAndParameters.returns({ fcn: 'initFunc', params: [] });
-            let res = await cc.Init(stub);
-            res.status.should.equal(Stub.RESPONSE_CODE.OK);
+            res = await cc.Init(stub);
+        });
+
+        it('should ping', async () => {
             stub.getFunctionAndParameters.returns({ fcn: 'invokeFunc', params: [] });
+            res = await cc.Invoke(stub);
+            res.status.should.equal(Stub.RESPONSE_CODE.OK);
+        });
+
+        it('should work for put', async () => {
+            stub.getFunctionAndParameters.returns({ fcn: 'put', params: ['one', 'two'] });
+            res = await cc.Invoke(stub);
+            res.status.should.equal(Stub.RESPONSE_CODE.OK);
+        });
+
+        it('should handle an error when calling put', async () => {
+            stub.getFunctionAndParameters.returns({ fcn: 'put', params: [] });
+            res = await cc.Invoke(stub);
+            res.status.should.equal(Stub.RESPONSE_CODE.ERROR);
+        });
+
+        it('should work for get', async () => {
+            stub.getFunctionAndParameters.returns({ fcn: 'get', params: ['one'] });
             res = await cc.Invoke(stub);
             res.status.should.equal(Stub.RESPONSE_CODE.OK);
         });
